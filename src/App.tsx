54fa034d19682afc04cdc4e6f8504df888ff408a
kiko0701 <32457861+kiko0701@users.noreply.github.com>
2019-02-25 15:19:40 +0900

    ReactNative+TypeScriptひな形

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Test from './components/test';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <Test></Test>
+    );
+  }
+}
