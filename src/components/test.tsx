f6bfc70771921ea66704345915cc724cd9e54636
kiko0701 <sol.tidus.rr@gmail.com>
2019-03-08 20:42:26 +0900

    コメント追記

--- src/components/test.tsx
+++ src/components/test.tsx
@@ -1,13 +1,13 @@
-import React, { FunctionComponent, useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { FunctionComponent, useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
 
 interface iTestProp {
   message: string;
 }
 
-
-export const Test: FunctionComponent<{ test?: iTestProp}> = ({test = {message: ''}}) => {
-  
+export const Test: FunctionComponent<{ test?: iTestProp }> = ({
+  test = { message: "" }
+}) => {
   const [message, setMessage] = useState(test);
 
   return (
@@ -15,13 +15,13 @@ export const Test: FunctionComponent<{ test?: iTestProp}> = ({test = {message: '
       <Text>{message}</Text>
     </View>
   );
-} 
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
 });
